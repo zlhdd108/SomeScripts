f31e9db2c84f3b4317d8b3bc57e8254fb62c4212
ooo <ooo@iMac.local>
2021-03-01 19:04:47 +0800

    fix telethon upgrade

--- Telethon/default_task.sh
+++ Telethon/default_task.sh
@@ -8,6 +8,11 @@ cd Telethon
 cp crontab_list.sh default_task.sh scripts_update.sh /pss
 echo "获取最新定时任务相关代码完成!"
 
+echo "获取最新pip及其模块"
+pip3 install --upgrade pip
+pip3 install --upgrade Telethon
+echo "获取最新pip及其模块完成"
+
 echo "执行脚本更新相关任务"
 sh -x /pss/scripts_update.sh
 echo "执行脚本更新相关任务完成!"
--- Telethon/docker_entrypoint.sh
+++ Telethon/docker_entrypoint.sh
@@ -8,11 +8,6 @@ cd Telethon
 cp crontab_list.sh default_task.sh scripts_update.sh /pss
 echo "获取最新定时任务相关代码完成"
 
-echo "获取最新pip及其模块"
-pip3 install --upgrade pip
-pip3 install --upgrade Telethon
-echo "获取最新pip及其模块完成"
-
 echo "首次初始化定时任务..."
 sh -x /pss/scripts_update.sh
 echo "初始化完成..."
