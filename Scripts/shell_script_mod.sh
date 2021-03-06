3f25034180f264d17e209523fc32f211f5a948af
ooo <ooo@iMac.local>
2021-03-07 03:12:28 +0800

    fix jd

--- Scripts/shell_script_mod.sh
+++ Scripts/shell_script_mod.sh
@@ -30,11 +30,11 @@ wget -O /scripts/jd_jump-jump.js https://raw.githubusercontent.com/i-chenzhe/qx/
 echo "# 母婴-跳一跳" >> /scripts/docker/merged_list_file.sh
 echo "5 8,14,20 2-7 3 * node /scripts/jd_jump-jump.js >> /scripts/logs/jd_jump-jump.log 2>&1" >> /scripts/docker/merged_list_file.sh
 # 超级摇一摇
-wget -O /scripts/jd_shake.js https://github.com/i-chenzhe/qx/blob/main/jd_shake.js
+wget -O /scripts/jd_shake.js https://raw.githubusercontent.com/i-chenzhe/qx/main/jd_shake.js
 echo "# 超级摇一摇" >> /scripts/docker/merged_list_file.sh
 echo "3 20 * * * node /scripts/jd_shake.js >> /scripts/logs/jd_shake.log 2>&1" >> /scripts/docker/merged_list_file.sh
 # 京东小魔方
-wget -O /scripts/jd_xmf.js https://github.com/i-chenzhe/qx/blob/main/jd_xmf.js
+wget -O /scripts/jd_xmf.js https://raw.githubusercontent.com/i-chenzhe/qx/main/jd_xmf.js
 echo "# 京东小魔方" >> /scripts/docker/merged_list_file.sh
 echo "10 10 * * * node /scripts/jd_xmf.js >> /scripts/logs/jd_xmf.log 2>&1" >> /scripts/docker/merged_list_file.sh
 
