9680a2b0f1a0d6ff6b0d4fad4fccd204f4943c34
ooo <ooo@iMac.local>
2021-03-10 01:54:36 +0800

    update UnicomTask

--- UnicomTask/docker_entrypoint.sh
+++ UnicomTask/docker_entrypoint.sh
@@ -6,19 +6,14 @@ if [ $1 ]; then
     run_cmd=$1
 fi
 
-echo "获取最新UnicomTask相关代码"
-git -C /scripts reset --hard
-git -C /scripts pull origin master --rebase
+cd /pss
+echo "获取最新定时任务相关代码..."
+git reset --hard
+git pull origin master --rebase
 
-git -C /UnicomTask reset --hard
-git -C /UnicomTask pull origin main --rebase
-echo "获取最新UnicomTask相关代码完成"
-cp -f /UnicomTask/*.py /DoUnicomTask
-cp -f /UnicomTask/requirements.txt /DoUnicomTask
-cd /DoUnicomTask && pip3 install --upgrade pip && pip3 install -r requirements.txt
-echo "首次初始化定时任务..."
-sh -x /scripts/UnicomTask/default_task.sh
-echo "初始化完成..."
+echo "------------------------------------------------执行定时任务任务shell脚本------------------------------------------------"
+sh -x /pss/UnicomTask/default_task.sh
+echo "--------------------------------------------------默认定时任务执行完成---------------------------------------------------"
 
 if [ $run_cmd ]; then
     echo "启动crondtab定时任务主进程..."
