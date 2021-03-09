4e2edbc2140b00519199f8a9960cf4dd1e469819
ooo <ooo@iMac.local>
2021-03-10 02:47:37 +0800

    fuck

--- UnicomTask/crontab_list.sh
+++ UnicomTask/crontab_list.sh
@@ -1,4 +1,4 @@
 # 每2天的23:50分清理一次日志
 50 23 */2 * * rm -rf /logs/*.log
 # UnicomTask
-30 23 * * * cd /scripts/UnicomTask/UnicomTask && python3 main.py
+30 23 * * * cd /scripts/UnicomTask && python3 main.py
