52baf2fd8e9f091c93353865fc6d1889ab3dec0d
ooo <ooo@iMac.local>
2021-03-09 23:59:51 +0800

    update UnicomTask

--- UnicomTask/docker_entrypoint.sh
+++ UnicomTask/docker_entrypoint.sh
@@ -10,8 +10,14 @@ echo "获取最新UnicomTask相关代码"
 cd /scripts
 git reset --hard
 git pull origin master --rebase
+cd /UnicomTaskScripts
+git reset --hard
+git pull origin main --rebase
+cp -f /UnicomTaskScripts/*.py /UnicomTask
+cp -f /UnicomTaskScripts/requirements.txt /UnicomTask
 cd /UnicomTask
-git pull
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
 echo "获取最新UnicomTask相关代码完成"
 
 echo "首次初始化定时任务..."
