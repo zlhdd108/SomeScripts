c77442b29c8897c9b2037e8bde0d7062815281d3
FKPYW <FKPYW>
2021-03-12 14:01:06 +0800

    fix

--- Telethon/scripts_update.sh
+++ Telethon/scripts_update.sh
@@ -6,7 +6,7 @@ if [ ! -f "/telethon/jd_zbj.py" ]; then
 else
     echo "京东直播间抽奖脚本存在，执行并重启脚本..."
     eval $(ps -ef | grep "jd_zbj" | grep -v "grep" | awk '{print "kill "$1}')
-    python3 -u /telethon/jd_zbj.py |ts >> /logs/jd_zbj.log 2>&1 &
+    cd /telethon && python3 -u jd_zbj.py |ts >> /logs/jd_zbj.log 2>&1 &
     echo "京东直播间抽奖脚本，重启完成..."
 fi
 
