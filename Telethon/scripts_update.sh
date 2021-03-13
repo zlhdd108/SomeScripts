bdb6d10903ddf28568775a36ae56d0b4988fbcc8
FKPYW <FKPYW>
2021-03-13 14:58:49 +0800

    update telethon sh

--- Telethon/scripts_update.sh
+++ Telethon/scripts_update.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 set -e
 
+PIDS=`ps -ef |grep jd_zbj |grep -v grep | awk '{print $2}'`
+
 if [ ! -f "/telethon/jd_zbj.py" ]; then
     echo "京东直播间抽奖脚本不存在，跳过执行..."
 else
-    echo "京东直播间抽奖脚本存在，执行并重启脚本..."
-    eval $(ps -ef | grep "jd_zbj" | grep -v "grep" | awk '{print "kill "$1}')
-    cd /telethon && python3 -u jd_zbj.py |ts >> /logs/jd_zbj.log 2>&1 &
-    echo "京东直播间抽奖脚本，重启完成..."
+    echo "京东直播间抽奖脚本存在，判断脚本进程是否存在..."
+    if [ "$PIDS" != "" ]; then
+        echo "脚本进程存在，跳过执行..."
+    else
+        echo "脚本进程不存在，执行脚本..."
+        cd /telethon && python3 -u jd_zbj.py |ts >> /logs/jd_zbj.log 2>&1 &
+        echo "脚本执行完成..."
+    fi
 fi
