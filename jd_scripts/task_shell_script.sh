fd9200d5b26fcb68f3e12e60efb72ed2fae66d83
FKPYW <FKPYW>
2021-03-15 01:49:16 +0800

    update jd

---
+++ jd_scripts/task_shell_script.sh
@@ -0,0 +1,129 @@
+#!/bin/sh
+set -e
+
+echo "定义定时任务合并处理用到的文件路径..."
+defaultListFile="/scripts/docker/$DEFAULT_LIST_FILE"
+echo "默认文件定时任务文件路径为 ${defaultListFile}"
+if [ $CUSTOM_LIST_FILE ]; then
+    customListFile="/scripts/docker/$CUSTOM_LIST_FILE"
+    echo "自定义定时任务文件路径为 ${customListFile}"
+fi
+mergedListFile="/scripts/docker/merged_list_file.sh"
+echo "合并后定时任务文件路径为 ${mergedListFile}"
+
+echo "第3步将默认定时任务列表添加到并后定时任务文件..."
+cat $defaultListFile >$mergedListFile
+
+echo "第2步判断是否存在自定义任务任务列表并追加..."
+if [ $CUSTOM_LIST_FILE ]; then
+    echo "您配置了自定义任务文件：$CUSTOM_LIST_FILE，自定义任务类型为：$CUSTOM_LIST_MERGE_TYPE..."
+    if [ -f "$customListFile" ]; then
+        if [ $CUSTOM_LIST_MERGE_TYPE == "append" ]; then
+            echo "合并默认定时任务文件：$DEFAULT_LIST_FILE 和 自定义定时任务文件：$CUSTOM_LIST_FILE"
+            echo -e "" >>$mergedListFile
+            cat $customListFile >>$mergedListFile
+        elif [ $CUSTOM_LIST_MERGE_TYPE == "overwrite" ]; then
+            echo "配置了自定义任务文件：$CUSTOM_LIST_FILE，自定义任务类型为：$CUSTOM_LIST_MERGE_TYPE..."
+            cat $customListFile >$mergedListFile
+        else
+            echo "配置配置了错误的自定义定时任务类型：$CUSTOM_LIST_MERGE_TYPE，自定义任务类型为只能为append或者overwrite..."
+        fi
+    else
+        echo "配置的自定义任务文件：$CUSTOM_LIST_FILE未找到，使用默认配置$DEFAULT_LIST_FILE..."
+    fi
+else
+    echo "当前只使用了默认定时任务文件 $DEFAULT_LIST_FILE ..."
+fi
+
+echo "第3步判断是否配置了默认脚本更新任务..."
+if [ $(grep -c "default_task.sh" $mergedListFile) -eq '0' ]; then
+    echo "合并后的定时任务文件，未包含必须的默认定时任务，增加默认定时任务..."
+    echo -e >>$mergedListFile
+    echo "21 */1 * * * sleep \$((RANDOM % \$RANDOM_DELAY_MAX)); docker_entrypoint.sh >> /scripts/logs/default_task.log 2>&1" >>$mergedListFile
+else
+    sed -i "/default_task.sh/d" $mergedListFile
+    echo "#脚本追加默认定时任务" >>$mergedListFile
+    echo "21 */1 * * * sleep \$((RANDOM % \$RANDOM_DELAY_MAX)); docker_entrypoint.sh >> /scripts/logs/default_task.log 2>&1" >>$mergedListFile
+fi
+
+echo "第5步判断是否配置了随即延迟参数..."
+if [ $RANDOM_DELAY_MAX ]; then
+    if [ $RANDOM_DELAY_MAX -ge 1 ]; then
+        echo "已设置随机延迟为 $RANDOM_DELAY_MAX , 设置延迟任务中..."
+        sed -i "/\(jd_bean_sign.js\|jd_blueCoin.js\|jd_5g.js\|jd_818.js\|jd_newYearMoney.js\|jd_newYearMoney_lottery.js\|jd_joy_reward.js\|jd_joy_steal.js\|jd_joy_feedPets.js\|jd_car_exchange.js\)/!s/node/sleep \$((RANDOM % \$RANDOM_DELAY_MAX)); node/g" $mergedListFile
+    fi
+else
+    echo "未配置随即延迟对应的环境变量，故不设置延迟任务..."
+fi
+
+echo "第6步判断是否配置自定义shell执行脚本..."
+if [ 0"$CUSTOM_SHELL_FILE" = "0" ]; then
+    echo "未配置自定shell脚本文件，跳过执行。"
+else
+    if expr "$CUSTOM_SHELL_FILE" : 'http.*' &>/dev/null; then
+        echo "自定义shell脚本为远程脚本，开始下在自定义远程脚本。"
+        wget -O /jds/shell_script_mod.sh $CUSTOM_SHELL_FILE
+        echo "下载完成，开始执行..."
+        echo "#远程自定义shell脚本追加定时任务" >>$mergedListFile
+        sh /jds/shell_script_mod.sh
+        echo "自定义远程shell脚本下载并执行结束。"
+    else
+        if [ ! -f $CUSTOM_SHELL_FILE ]; then
+            echo "自定义shell脚本为docker挂载脚本文件，但是指定挂载文件不存在，跳过执行。"
+        else
+            echo "docker挂载的自定shell脚本，开始执行..."
+            echo "#docker挂载自定义shell脚本追加定时任务" >>$mergedListFile
+            sh $CUSTOM_SHELL_FILE
+            echo "docker挂载的自定shell脚本，执行结束。"
+        fi
+    fi
+fi
+
+echo "第7步增加 |ts 任务日志输出时间戳..."
+sed -i "/\( ts\| |ts\|| ts\)/!s/>>/\|ts >>/g" $mergedListFile
+
+sed -i "/\(>&1 &\|> &1 &\)/!s/>&1/>\&1 \&/g" $mergedListFile
+
+echo "第8步执行原仓库的附属脚本proc_file.sh"
+sh /scripts/docker/proc_file.sh
+
+echo "第9步加载最新的定时任务文件..."
+crontab $mergedListFile
+# cp /local_scripts/cookies*.sh  /root/
+# source /root/cookies*.sh
+
+echo "第10步将仓库的docker_entrypoint.sh脚本更新至系统/usr/local/bin/docker_entrypoint.sh内..."
+cat /jds/jd_scripts/docker_entrypoint.sh >/usr/local/bin/docker_entrypoint.sh
+
+if [ $GEN_CODE_CONF ]; then
+    cp /jds/jd_scripts/gen_code_conf.list $GEN_CODE_CONF
+fi 
+
+if [ -d '/local_scripts/LiveLotteryForward/' ];then
+    cd /local_scripts/LiveLotteryForward/
+    pip3 install -r requirements.txt
+    sh liveLottery.sh start
+fi
+
+# echo "第11步打包脚本文件到/scripts/logs/scripts.tar.gz"
+# apk add tar
+# tar -zcvf /scripts/logs/scripts.tar.gz --exclude=scripts/node_modules --exclude=scripts/logs/*.log  --exclude=scripts/logs/*.gz /scripts
+
+# 京喜工厂参团
+sed -i "s/https:\/\/gitee.com\/shylocks\/updateTeam\/raw\/main\/jd_updateFactoryTuanId.json/https:\/\/raw.githubusercontent.com\/FKPYW\/updateTeam\/master\/shareCodes\/jd_updateFactoryTuanId.json/g" /scripts/jd_dreamFactory.js
+sed -i "s/https:\/\/raw.githubusercontent.com\/LXK9301\/updateTeam\/master\/jd_updateFactoryTuanId.json/https:\/\/raw.githubusercontent.com\/FKPYW\/updateTeam\/master\/shareCodes\/jd_updateFactoryTuanId.json/g" /scripts/jd_dreamFactory.js
+sed -i "s/https:\/\/gitee.com\/lxk0301\/updateTeam\/raw\/master\/shareCodes\/jd_updateFactoryTuanId.json/https:\/\/raw.githubusercontent.com\/FKPYW\/updateTeam\/master\/shareCodes\/jd_updateFactoryTuanId.json/g" /scripts/jd_dreamFactory.js
+sed -i "s/\(.*\/\/.*joinLeaderTuan.*\)/  await joinLeaderTuan();/g" /scripts/jd_dreamFactory.js
+# 京东赚赚
+sed -i "s/https:\/\/gitee.com\/shylocks\/updateTeam\/raw\/main\/jd_zz.json/https:\/\/raw.githubusercontent.com\/FKPYW\/updateTeam\/master\/shareCodes\/jd_zz.json/g" /scripts/jd_jdzz.js
+sed -i "s/https:\/\/gitee.com\/lxk0301\/updateTeam\/raw\/master\/shareCodes\/jd_zz.json/https:\/\/raw.githubusercontent.com\/FKPYW\/updateTeam\/master\/shareCodes\/jd_zz.json/g" /scripts/jd_jdzz.js
+# 签到领现金
+sed -i "s/https:\/\/gitee.com\/shylocks\/updateTeam\/raw\/main\/jd_cash.json/https:\/\/raw.githubusercontent.com\/FKPYW\/updateTeam\/master\/shareCodes\/jd_updateCash.json/g" /scripts/jd_cash.js
+sed -i "s/https:\/\/gitee.com\/lxk0301\/updateTeam\/raw\/master\/shareCodes\/jd_updateCash.json/https:\/\/raw.githubusercontent.com\/FKPYW\/updateTeam\/master\/shareCodes\/jd_updateCash.json/g" /scripts/jd_cash.js
+sed -ie "32,33s/^[^\]*/  \`eU9YaOvnM_4k9WrcnnAT1Q@eU9Yar-3M_8v9WndniAQhA@eU9YaO-2YPUn-TzQwycVgw@eU9YaOiwMKklo2eGyCFG1A@ZnQya-i1Y_UmpGzUnnEX@fkFwauq3ZA@aUNmM6_nOP4j-W4@eU9Yau3kZ_4g-DiByHEQ0A@f0JyJuW7bvQ@IhM0bu-0b_kv8W6E@eU9YKpnxOLhYtQSygTJQ@-oaWtXEHOrT_bNMMVso@eU9YG7XaD4lXsR2krgpG\`,/g" /scripts/jd_cash.js
+# 宠汪汪赛跑邀请助力
+cf -f /scripts/jd_joy_run.js /jds/jd_scripts/scripts/jd_joy_run.js
+# 修改助力开关
+sed -i "s/helpAu = true;/helpAu = false;/g" /scripts/jd_*.js
+sed -i "s/helpAuthor = true;/helpAuthor = false;/g" /scripts/jd_*.js
+
