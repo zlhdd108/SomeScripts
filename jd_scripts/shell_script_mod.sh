699cb5bd3e8aec4ef11b0e0a7647917731a76f72
FKPYW <FKPYW>
2021-03-16 19:06:01 +0800

    update

--- jd_scripts/shell_script_mod.sh
+++ jd_scripts/shell_script_mod.sh
@@ -10,21 +10,19 @@ else
     git -C /i-chenzhe pull --rebase
 fi
 # 克隆jd_scripts仓库
-if [ ! -d "/jd_scripts/" ]; then
-    echo "未检查到jd_scripts仓库脚本，初始化下载相关脚本..."
-    git clone https://github.com/FKPYW/SomeScripts.git /jd_scripts
+if [ ! -d "/SomeScripts/" ]; then
+    echo "未检查到SomeScripts仓库脚本，初始化下载相关脚本..."
+    git clone https://github.com/FKPYW/SomeScripts.git /SomeScripts
 else
     echo "更新jd_scripts脚本相关文件..."
-    cd /jd_scripts
-    git remote set-url origin https://github.com/FKPYW/SomeScripts.git 
-    git -C /jd_scripts reset --hard
-    git -C /jd_scripts pull --rebase
+    git -C /SomeScripts reset --hard
+    git -C /SomeScripts pull --rebase
 fi
 
 cp -f /i-chenzhe/*.js /scripts
-cp -f /jd_scripts/jd_scripts/*.js /scripts/docker
-cp -f /jd_scripts/jd_scripts/diy.sh /scripts/docker
-cp -f /jd_scripts/jd_scripts/custom_script.sh /scripts/docker
+cp -f /SomeScripts/jd_scripts/*.js /scripts/docker
+cp -f /SomeScripts/jd_scripts/diy.sh /scripts/docker
+cp -f /SomeScripts/jd_scripts/custom_script.sh /scripts/docker
 
 # custom_docker_entrypoint
 echo "# 自定义更新时间" >> /scripts/docker/merged_list_file.sh
