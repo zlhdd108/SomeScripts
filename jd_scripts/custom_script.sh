53aed4adbc3df7e253706fb6c3ed511304b41873
ooo <ooo@iMac.local>
2021-03-17 23:19:59 +0800

    update

--- jd_scripts/custom_script.sh
+++ jd_scripts/custom_script.sh
@@ -13,7 +13,7 @@ sed -i "s/https:\/\/gitee.com\/shylocks\/updateTeam\/raw\/main\/jd_cash.json/htt
 sed -i "s/https:\/\/gitee.com\/lxk0301\/updateTeam\/raw\/master\/shareCodes\/jd_updateCash.json/https:\/\/raw.githubusercontent.com\/FKPYW\/updateTeam\/master\/shareCodes\/jd_updateCash.json/g" /scripts/jd_cash.js
 sed -ie "32,33s/^[^\]*/  \`eU9YaOvnM_4k9WrcnnAT1Q@eU9Yar-3M_8v9WndniAQhA@eU9YaO-2YPUn-TzQwycVgw@eU9YaOiwMKklo2eGyCFG1A@ZnQya-i1Y_UmpGzUnnEX@fkFwauq3ZA@aUNmM6_nOP4j-W4@eU9Yau3kZ_4g-DiByHEQ0A@f0JyJuW7bvQ@IhM0bu-0b_kv8W6E@eU9YKpnxOLhYtQSygTJQ@-oaWtXEHOrT_bNMMVso@eU9YG7XaD4lXsR2krgpG\`,/g" /scripts/jd_cash.js
 # 宠汪汪赛跑
-cp -f /scripts/docker/jd_joy_run.js /scripts/jd_joy_run.js 
+cp -f /scripts/docker/jd_joy_run.js /scripts/jd_joy_run.js
 # 修改助力开关
 sed -i "s/helpAu = true;/helpAu = false;/g" /scripts/jd_*.js
 sed -i "s/helpAuthor = true;/helpAuthor = false;/g" /scripts/jd_*.js
