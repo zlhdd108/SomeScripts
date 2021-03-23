938c47a91f14ad60fff87081e92964823f1ee6c8
ooo <ooo@iMac.local>
2021-03-23 18:32:12 +0800

    update jd

--- jd_scripts/shell_script_mod.sh
+++ jd_scripts/shell_script_mod.sh
@@ -12,11 +12,3 @@ fi
 cp -f /i-chenzhe/*_*.js /scripts
 
 cat /i-chenzhe/remote_crontab_list.sh >> /scripts/docker/merged_list_file.sh
-
-if [ ! -f "/scripts/jd_live_lottery_social.js" ];then
-    echo "未检查到live脚本..."
-    else
-    echo "查到live脚本..."
-    echo "# if" >> /scripts/docker/merged_list_file.sh
-    echo "5 8,12,16,20,23 * * * node /scripts/jd_live_lottery_social.js >> /scripts/logs/jd_live_lottery_social.log 2>&1" >> /scripts/docker/merged_list_file.sh
-fi
